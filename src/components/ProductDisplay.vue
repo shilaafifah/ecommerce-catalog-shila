2542f2d46a6432f5d1a6e486de4343a88a144b9d
Shila Afifah <shilaafifah@gmail.com>
2023-10-01 14:56:04 +0700

    Edit File ProductDisplay

--- src/components/ProductDisplay.vue
+++ src/components/ProductDisplay.vue
@@ -17,7 +17,9 @@
             </div>
           </div>
         </div>
-        <div v-else-if="loading" class="loader"> loading</div>
+        <div v-else-if="loading">
+          <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
+       </div>
         <div v-else>
           <div class="product-container">
             <div class="product-image">
@@ -288,6 +290,41 @@
     width: 500px;
     height: 50px;
     margin-left: 183px;
+  }
+  .lds-ring {
+    display: inline-block;
+    position: relative;
+    width: 80px;
+    height: 80px;
+  }
+  .lds-ring div {
+    box-sizing: border-box;
+    display: block;
+    position: absolute;
+    width: 64px;
+    height: 64px;
+    margin: 8px;
+    border: 8px solid #8d8d8d;
+    border-radius: 50%;
+    animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+    border-color: #8d8d8d transparent transparent transparent;
+  }
+  .lds-ring div:nth-child(1) {
+    animation-delay: -0.45s;
+  }
+  .lds-ring div:nth-child(2) {
+    animation-delay: -0.3s;
+  }
+  .lds-ring div:nth-child(3) {
+    animation-delay: -0.15s;
+  }
+  @keyframes lds-ring {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
   }
   
   </style>
